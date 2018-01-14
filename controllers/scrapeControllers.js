246b6b321ef3fe72bda81c3fe55f0b1d5b76cba1
adamtre9115 <treadam9115@gmail.com>
2018-01-14 18:50:14 -0500

    add functionality to render articles to page

--- controllers/scrapeControllers.js
+++ controllers/scrapeControllers.js
@@ -13,43 +13,65 @@ app.get("/", (req, res) => {
 // scrape 
 app.get("/scrape", (req, res) => {
 
-    // make request to clt observer
-    request("http://www.charlotteobserver.com/news/local/", function (err, response, html) {
+    function getArticles() {
+        return new Promise(function(resolve) {
 
-        // Load the html body from request into cheerio
-        var $ = cheerio.load(html);
+            request("http://www.charlotteobserver.com/news/local/", function(err, response, html) {
 
-        $("article").each(function (i, element) {
-            var title = $(element).children('.teaser').children('.title').children("a").text();
-            var link = $(element).children('.teaser').children('.title').children("a").attr("href");
-            var summary = $(element).children('.teaser').children('.summary').text();
+                if (!err) {
+                    // Load the html body from request into cheerio
+                    var $ = cheerio.load(html);
 
-            // If this found element had a title, link and, summary
-            // length greater than 17 because it excluedes button links that are returned
-            if (title.length > 17 && link && summary) {
-                // create a new object frome the articles
-                var scrapedData = {
-                    title: title,
-                    link: link,
-                    summary: summary
+                    $("article").each(function(i, element) {
+                        var title = $(element).children('.teaser').children('.title').children("a").text();
+                        var link = $(element).children('.teaser').children('.title').children("a").attr("href");
+                        var summary = $(element).children('.teaser').children('.summary').text();
+
+                        // If this found element had a title, link and, summary
+                        // length greater than 17 because it excluedes button links that are returned
+                        if (title.length > 17 && link && summary) {
+                            // create a new object frome the articles
+                            var scrapedData = {
+                                title: title,
+                                link: link,
+                                summary: summary
+                            }
+
+                            // Insert the data in the scrapedData db
+                            var scrapedArticles = new articles(scrapedData);
+
+                            scrapedArticles.save(function(error, doc) {
+                                // promise has been fulfilled if no errors
+                                if (!error) {
+                                    resolve()
+                                } else {
+                                    console.log(error)
+                                }
+                            });
+                        }
+                    })
+                } else {
+                    console.log(err)
                 }
+            })
+        })
+    }
 
-                // Insert the data in the scrapedData db
-                /* var scrapedArticles = new articles(scrapedData);
-
-                 scrapedArticles.save(function (error, doc) {
-                     // Send any errors to the browser
-                     if (error) {
-                         console.log(error);
-                     }
-                     // Otherwise, send the new doc to the browser
-                     else {
-                         console.log("articles sent to db");
-                     }
-                 });*/
+    getArticles().then(() => {
+        // get all articles from db
+        articles.find({}, function(err, docs) {
+            if (!err) {
+                // place them in an object
+                var newArticles = {
+                    articles: docs
+                }
+                // send articles to front in page render
+                res.render("index", newArticles)
 
             }
+
         })
+
     })
 })
 
